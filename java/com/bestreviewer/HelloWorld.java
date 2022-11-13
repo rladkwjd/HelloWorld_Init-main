431dbd08c832f353e09ccb0afbbcae0fe25c22e5
rladkwjd <bada0369@naver.com>
2022-11-13 19:57:16 +0900

    Initial commit

---
+++ java/com/bestreviewer/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.bestreviewer;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Java");
+    }
+    public String sayHello() {
+
+        return "Hello";
+    }
+}
